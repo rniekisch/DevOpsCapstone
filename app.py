ef9fb545a89bae48086500e8963845fd683e1bbc
Roland Niekisch <roland.niekisch@t-online.de>
2021-07-05 15:56:50 +0200

    Add function for retrieving geo information

--- app.py
+++ app.py
@@ -1,6 +1,38 @@
 from flask import Flask, request, jsonify
 from flask.logging import create_logger
+from json import loads
+from re import compile, VERBOSE
+from urllib import urlopen
 import logging
+
+FREE_GEOIP_URL = "http://freegeoip.net/json/{}"
+VALID_IP = compile(r"""
+\b
+(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)
+\b
+""", VERBOSE)
+
+def get_geodata(ip):
+    """
+    Search for geolocation information using http://freegeoip.net/
+    """
+    if not VALID_IP.match(ip):
+        raise ValueError('Invalid IPv4 format')
+
+    url = FREE_GEOIP_URL.format(ip)
+    data = {}
+
+    try:
+        response = urlopen(url).read()
+        data = loads(response)
+    except Exception:
+        pass
+
+    return data
+
 
 app = Flask(__name__)
 LOG = create_logger(app)
@@ -15,7 +47,6 @@ def home():
     geodata = get_geodata(ip_address)
     location = "{}, {}".format(geodata.get("city"),
                                geodata.get("zipcode"))
-
     return '''
 <html>
     <head>
