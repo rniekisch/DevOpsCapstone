d9f41148203a837861fdb154d30a50fa07c67fae
Roland Niekisch <roland.niekisch@t-online.de>
2021-07-05 17:28:26 +0200

    Fix wrong formatting

--- app.py
+++ app.py
@@ -9,12 +9,11 @@ LOG.setLevel(logging.INFO)
 
 @app.route("/")
 def home():
-    browser = request.headers.get("User-Agent")
-    url = request.values.get("url") or request.headers.get("Referer")
-    event = request.values.get("event")
-    ip_address = request.remote_addr
-    if ip_address is None:
-        ip_address = 'unknown'
+    d = dict()
+    d['browser'] = request.headers.get("User-Agent")
+    d['url'] = request.values.get("url") or request.headers.get("Referer")
+    d['event'] = request.values.get("event")
+    d['ipaddress'] = request.remote_addr
     return '''
 <html>
     <head>
@@ -22,13 +21,13 @@ def home():
     </head>
     <body>
         <table>
-            <tr> <td>Browser:</td> <td>'''+browser+'''</td> </tr>
-            <tr> <td>Url:</td> <td>'''+url+'''</td> </tr>
-            <tr> <td>Event:</td> <td>'''+event+'''</td> </tr>
-            <tr> <td>IP Adress:</td> <td>'''+ip_address+'''</td> </tr>
+            <tr> <td>Browser:</td> <td>{browser}</td> </tr>
+            <tr> <td>Url:</td> <td>{url}</td> </tr>
+            <tr> <td>Event:</td> <td>{event}</td> </tr>
+            <tr> <td>IP Adress:</td> <td>{ipaddress}</td> </tr>
         </table>
     </body>
-</html>'''
+</html>'''.format(**d)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True) # specify port=80
